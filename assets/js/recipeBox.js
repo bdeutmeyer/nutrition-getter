e63ef889487c4be4cb0771ff06395a91cd9e5829
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-03 11:55:10 -0500

    Recipe box styling and functionality, main page body styling

--- assets/js/recipeBox.js
+++ assets/js/recipeBox.js
@@ -2,22 +2,34 @@ var modalTestBtn = document.getElementById('modal-test-btn');
 var recipeBoxContainerEl = document.getElementById('recipe-box-container');
 var recipeBoxHeaderEl = document.getElementById('recipe-box-header');
 var recipeCardForm = document.getElementById('recipe-card-form');
+var titleInputEl = document.getElementById('title-input');
+var recipeBodyEl = document.getElementById('recipe-body');
+var recipeUrl = document.getElementById('recipe-url');
 var closeBtn = document.getElementById('close');
 var saveBtn = document.getElementById('save-button');
+var recipeCardArray = [];
 
-function saveRecipe() {
-    
-}
 
 modalTestBtn.onclick = function() {
     recipeCardForm.style.display = 'block';
 }
 
-closeBtn.onclick = function() {
+closeBtn.onclick = function(event) {
+    event.stopPropagation();
     recipeCardForm.style.display = 'none';
 }
 
-saveBtn.onclick = function() {
-    saveRecipe();
+recipeCardForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var newRecipe = {
+        title: titleInputEl.value,
+        body: recipeBodyEl.value,
+        url: recipeUrl.value,
+        // nutInfo: nutInfo
+    }
+
+    recipeCardArray.push(newRecipe);
+    localStorage.setItem('recipe', JSON.stringify(recipeCardArray));
     recipeCardForm.style.display = 'none';
-}
+});
