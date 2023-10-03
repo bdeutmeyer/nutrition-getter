c4dfba4a1be5c86de81f28c2f17eeb2d19ac4290
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-03 10:08:35 -0500

    more recipe box work

--- assets/js/recipeBox.js
+++ assets/js/recipeBox.js
@@ -3,7 +3,11 @@ var recipeBoxContainerEl = document.getElementById('recipe-box-container');
 var recipeBoxHeaderEl = document.getElementById('recipe-box-header');
 var recipeCardForm = document.getElementById('recipe-card-form');
 var closeBtn = document.getElementById('close');
+var saveBtn = document.getElementById('save-button');
 
+function saveRecipe() {
+    
+}
 
 modalTestBtn.onclick = function() {
     recipeCardForm.style.display = 'block';
@@ -11,5 +15,9 @@ modalTestBtn.onclick = function() {
 
 closeBtn.onclick = function() {
     recipeCardForm.style.display = 'none';
+}
 
+saveBtn.onclick = function() {
+    saveRecipe();
+    recipeCardForm.style.display = 'none';
 }
